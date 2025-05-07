af86e102d5da135c50ac7885527c6765cf8bf363
Jiao <zajiao2024@hotmail.com>
2025-05-07 10:48:30 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def print_welcome():
+    print("Hello, Git!")
+    print("这是我的第一个Python项目")
+
+def print_advanced():
+    print("=" * 30)
+    print("这是在新分支上添加的高级打印功能")
+    print("我们在学习Git分支管理")
+    print("=" * 30)
+
+def main():
+    print_welcome()
+    print_advanced()
+
+if __name__ == "__main__":
+    main()
